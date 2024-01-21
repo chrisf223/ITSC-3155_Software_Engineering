74a4927c42fe76b1f95740f6c1b9db7b7b74163c
chrisf223 <chrisfranceschini23@gmail.com>
2024-01-21 00:45:13 -0500

    added script 3

--- script_02.py
+++ script_02.py
@@ -18,4 +18,4 @@ def get_combined_dict(dict1, dict2):
 my_dict_1 = {'a': 5, 'b': 12, 'c': 3, 'd': 9}
 my_dict_2 = {'b': 4, 'c': 9, 'd': 10, 'e': 16}
 combined_dict = get_combined_dict(my_dict_1, my_dict_2)
-print(combined_dict)
+print("Combined Dict: " + str(combined_dict))
