f4d1db4cf2b7d0be4eb9eb24b311a680f4d06ae3
chrisf223 <145400430+chrisf223@users.noreply.github.com>
2024-01-31 13:14:22 -0500

    exercise 3

--- exercise_03/main.py
+++ exercise_03/main.py
@@ -1,3 +1,8 @@
+## Create a virtual environment (with any name) and activate it. Install the numpy package in this virtual environment (pip install numpy). 
+## Numpy is a statistical package used for matrices and linear algebra and is vital for data analysis, data science, machine learning, etc. in Python. 
+## Use the internet and online documentation to use numpy to create a numpy list of 10 random floats, each 0 to 1, then print the mean, median, and standard deviation of that list, all using numpy. 
+## Remember to add a requirements.txt file to this exercise folder, commit it, but do NOT commit the virtual environment folder.
+
 import numpy as np
 
 random_floats = np.random.rand(10)
